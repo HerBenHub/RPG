7a42169dcc44bd435bddd3855f8d8078161564b9
herben <heredibenedek@gmail.com>
2024-02-29 12:46:00 +0100

    starting class

--- rpeg_game/Program.cs
+++ rpeg_game/Program.cs
@@ -1,6 +1,16 @@
 ﻿internal class Program
 {
-    
+    public class Characters
+    {
+        public string name;
+        public int hp;
+        public int defense; //defense
+        public int weapon; //attack
+        public int armour;
+
+        public int damage; //if enemy
+
+    }
     public static void Main(string[] args)
     {
         

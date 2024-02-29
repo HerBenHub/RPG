fcf14993db51da676ea3bf2d9733fea3f03e2116
herben <heredibenedek@gmail.com>
2024-02-29 13:05:39 +0100

    alapötlet felépítésre

--- rpeg_game/Program.cs
+++ rpeg_game/Program.cs
@@ -8,8 +8,26 @@
         public int weapon; //attack
         public int armour;
 
+        public void levelUp()
+        {
+            hp += 10;
+            defense += 10;
+            weapon += 10;//actual weapon damage + 10
+        }
+    }
+    
+    //Ha ügyesek vagyunk akkor jó lesz. Szóval itt öröklődnek a Character class-ból a
+    //tulajdonsáhok tovább: Mate, Enemy osztályba. Ahol csak az adott csoportra jellemző tulajdonságok lesznek
+    //ott lehet látni pluszban még functionoket: public int damage. Mert ez csak az enemy tulajdonsága lesz
+    public class Mate : Characters
+    {
+        //Mates here
+    }
+    public class Enemy : Characters
+    {
         public int damage; //if enemy
-
+        
+        //Enemys here
     }
     public static void Main(string[] args)
     {

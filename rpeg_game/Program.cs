e40c5fdccb7745f3e49492211aaf1f95ed06670f
herben <heredibenedek@gmail.com>
2024-02-29 09:33:41 +0100

    kezdés

--- rpeg_game/Program.cs
+++ rpeg_game/Program.cs
@@ -1,4 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("HEllo WRLD");
+﻿internal class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+//AZ rpg játék...
+//Feladatok kiosztása
+//Csapatmunka összehozása (mikor, mennyit, mit)
+//Következő: játékmenet kiötletelése (menet közben nagyban módusulhat)

6e8addf864a83ded81c78d8e45d35a51245d68b7
FelX023 <161696630+FelX023@users.noreply.github.com>
2024-03-07 11:50:53 +0100

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace menutest
+{
+    class Game
+    {
+        public void Start()
+        {
+            Title = "Dark RPG";
+            FoMenufuttatas();
+
+        }
+        private void FoMenufuttatas()
+        {
+            string prompt = @"
+██████╗  █████╗ ██████╗ ██╗  ██╗    ██████╗ ██████╗  ██████╗ 
+██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝    ██╔══██╗██╔══██╗██╔════╝ 
+██║  ██║███████║██████╔╝█████╔╝     ██████╔╝██████╔╝██║  ███╗
+██║  ██║██╔══██║██╔══██╗██╔═██╗     ██╔══██╗██╔═══╝ ██║   ██║
+██████╔╝██║  ██║██║  ██║██║  ██╗    ██║  ██║██║     ╚██████╔╝
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝      ╚═════╝                                                              
+
+Üdvözöllek a játékban! Válassz a nyilakkal.";
+            string[] opciok = { "új játék", "folytatás", "Kilépés" };
+            Menu mainMenu = new Menu(prompt, opciok);
+            int valasztott = mainMenu.futtatas();
+
+            switch (valasztott) 
+            {
+                case 0:
+                    Ujjatek();
+                    break;
+                case 1:
+                    Folytatas();
+                    break;
+                case 2:
+                    Kilepes();
+                    break;
+
+            }
+        }
+
+        private void Kilepes()
+        {
+            string prompt = "Biztosan ki szeretnél lépni?\n";
+            string[] opciok = { "Igen", "Nem" };
+            Menu E_Menu = new Menu(prompt, opciok);
+            int E_valasztott = E_Menu.futtatas();
+
+            BackgroundColor = ConsoleColor.Black;
+            switch(E_valasztott)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    FoMenufuttatas();
+                    break;
+            }
+        }
+
+        private void Ujjatek()
+        {
+            WriteLine("\nMég nincs itt semmi");
+            ReadKey(true);
+            FoMenufuttatas();
+        }
+
+        private void Folytatas()
+        {
+            WriteLine("\nMég itt sincs semmi");
+            ReadKey(true);
+            FoMenufuttatas();
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace menutest
+{
+    internal class Menu
+    {
+        private int valasztott;
+        private string[] Opciok;
+        private string Prompt;
+
+        public Menu(string prompt, string[] opciok)
+        {
+            Prompt = prompt;
+            Opciok = opciok;
+            valasztott = 0;
+        }
+
+        private void DisplayOpciok()
+        {
+            WriteLine(Prompt);
+            for (int i = 0; i < Opciok.Length; i++)
+            {
+                string jelenlegiopcio = Opciok[i];
+                string jel;
+
+                if (i == valasztott)
+                {
+                    // megváltozik a színe a kiválasztott index (opció) nek 
+                    jel = "*";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    jel = " ";
+                    ForegroundColor = ConsoleColor.Gray;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                WriteLine($"{jel}  {jelenlegiopcio}");
+            }
+            ResetColor();
+        }
+
+        public int futtatas()
+        {
+            ConsoleKey nyomott;
+            //amíg a felhasználó nem nyomja meg az Entert addig nem megy tovább
+            do
+            {
+                Clear();
+                DisplayOpciok();
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                nyomott = keyInfo.Key;
+                //ne menjen az érték minuszba
+                if(nyomott == ConsoleKey.UpArrow)
+                {
+                    valasztott--;
+                    if(valasztott == -1)
+                    {
+                        valasztott = Opciok.Length - 1;
+                    }
+                }
+                else if (nyomott == ConsoleKey.DownArrow)
+                {
+                    //ne menjen túl a maximum indexen(opción)
+                    valasztott++;
+                    if (valasztott == Opciok.Length)
+                    {
+                        valasztott = 0;
+                    }
+                }
+            } while (nyomott != ConsoleKey.Enter);
+
+            return valasztott;
+        }
+    }
+}
